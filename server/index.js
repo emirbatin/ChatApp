03b4070b29323f5546e2da0e4f6bdc8e779bac53
Batın <58427283+emirbatin@users.noreply.github.com>
2024-07-23 21:27:27 +0300

    index.js updated

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption = {
-  origin: ["http://localhost:3000", "https://chatappclient-six.vercel.app"],
+  origin: ["http://localhost:3000"],
   methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
