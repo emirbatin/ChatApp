6527c8f41b1c0a3f6598ab470a0f77acd0180641
Batın <58427283+emirbatin@users.noreply.github.com>
2024-07-23 18:40:02 +0300

    vercel ayarları güncellendi

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,21 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
-  optimizeDeps: {
-    include: ["react-hot-toast"],
   },
   plugins: [react()],
 });
