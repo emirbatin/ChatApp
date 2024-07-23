d79d09cd5460964a8157078d91d5ead6ee52641b
Batın <58427283+emirbatin@users.noreply.github.com>
2024-07-24 02:14:09 +0300

    url güncellendi

--- client/src/services/socketService.jsx
+++ client/src/services/socketService.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 let socket;
 
 export const initializeSocket = (userId) => {
-  socket = io("http://localhost:4000", {
+  socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000", {
     query: { userId },
   });
   return socket;
