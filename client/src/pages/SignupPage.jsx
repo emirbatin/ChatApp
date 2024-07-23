7c233f3f93b5ee032f9a9c68682aee7e3667a74c
Batın <58427283+emirbatin@users.noreply.github.com>
2024-07-23 17:40:41 +0300

    ChatApp Deploy Hazır

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -14,9 +14,15 @@ const Signup = () => {
     gender: "",
   });
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
   const handleCheckbox = (gender) => {
     setUser({ ...user, gender });
   };
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
@@ -42,69 +48,85 @@ const Signup = () => {
       gender: "",
     });
   };
+
   return (
     <div className="min-w-[32rem] mx-auto">
       <div className="w-full p-0 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
         <h1 className="text-3xl font-bold text-start">Signup</h1>
-        <Spacer y={4}/>
-        <form onSubmit={onSubmitHandler} action="">
+        <Spacer y={4} />
+        <form onSubmit={onSubmitHandler}>
           <div>
             <Input
+              name="fullName"
               autoFocus
               label="Full Name"
               placeholder="Enter your full name"
               variant="bordered"
               value={user.fullName}
-              onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+              onChange={handleChange}
+              required
             />
           </div>
-          <Spacer y={4}/>
+          <Spacer y={4} />
           <div>
             <Input
-              autoFocus
+              name="username"
               label="Username"
               placeholder="Enter your username"
               variant="bordered"
               value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              onChange={handleChange}
+              required
             />
           </div>
-          <Spacer y={4}/>
+          <Spacer y={4} />
           <div>
             <Input
+              name="password"
               label="Password"
               placeholder="Enter your password"
               type="password"
               variant="bordered"
               value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              onChange={handleChange}
+              required
             />
           </div>
-          <Spacer y={4}/>
+          <Spacer y={4} />
           <div>
             <Input
+              name="confirmPassword"
               label="Confirm Password"
               placeholder="Please confirm your password"
               type="password"
               variant="bordered"
               value={user.confirmPassword}
-              onChange={(e) =>
-                setUser({ ...user, confirmPassword: e.target.value })
-              }
+              onChange={handleChange}
+              required
             />
           </div>
-          <Spacer y={4}/>
+          <Spacer y={4} />
           <div>
-          <Button
-              type="submit"
-              color="primary"
+            <select
+              name="gender"
+              onChange={handleChange}
+              value={user.gender}
+              required
             >
+              <option value="" disabled>Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <Spacer y={4} />
+          <div>
+            <Button type="submit" color="primary">
               Signup
             </Button>
           </div>
-          <Spacer y={4}/>
+          <Spacer y={4} />
           <p className="text-sm text-center my-2">
-            Already have an account? <Link to="/login" className="text-blue-500"> Login </Link>
+            Already have an account? <Link to="/login" className="text-blue-500">Login</Link>
           </p>
         </form>
       </div>
