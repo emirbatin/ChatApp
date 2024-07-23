3828730aa4ab143b5cf4def60e58e961924d90a0
Batın <58427283+emirbatin@users.noreply.github.com>
2024-07-23 23:20:17 +0300

    controller updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,6 @@ import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-// Registration
 export const register = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -10,19 +9,18 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
     if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords do not match" });
+      return res.status(400).json({ message: "Password do not match" });
     }
 
     const user = await User.findOne({ username });
     if (user) {
       return res
         .status(400)
-        .json({ message: "Username already exists, try a different one" });
+        .json({ message: "Username already exit try different" });
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Profile Photo
+    // profilePhoto
     const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
@@ -33,47 +31,46 @@ export const register = async (req, res) => {
       profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
       gender,
     });
-
-    return res
-      .status(201)
-      .json({ message: "Account created successfully.", success: true });
+    return res.status(201).json({
+      message: "Account created successfully.",
+      success: true,
+    });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    console.log(error);
   }
 };
-
-// Login
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
     const user = await User.findOne({ username });
     if (!user) {
-      return res
-        .status(400)
-        .json({ message: "Incorrect username or password", success: false });
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
     }
-
     const isPasswordMatch = await bcrypt.compare(password, user.password);
     if (!isPasswordMatch) {
-      return res
-        .status(400)
-        .json({ message: "Incorrect username or password", success: false });
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
     }
+    const tokenData = {
+      userId: user._id,
+    };
 
-    const tokenData = { userId: user._id };
-    const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
       expiresIn: "1d",
     });
 
     return res
       .status(200)
       .cookie("token", token, {
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
       })
@@ -84,25 +81,18 @@ export const login = async (req, res) => {
         profilePhoto: user.profilePhoto,
       });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    console.log(error);
   }
 };
-
-// Logout
 export const logout = (req, res) => {
   try {
-    return res
-      .status(200)
-      .cookie("token", "", { maxAge: 0 })
-      .json({ message: "Logged out successfully." });
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "logged out successfully.",
+    });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    console.log(error);
   }
 };
-
-// Get Other Users
 export const getOtherUsers = async (req, res) => {
   try {
     const loggedInUserId = req.id;
@@ -111,7 +101,6 @@ export const getOtherUsers = async (req, res) => {
     );
     return res.status(200).json(otherUsers);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    console.log(error);
   }
 };
